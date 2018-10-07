cb51ef4a067337e23880d097980e5c8af24db17f
Kat Michaela <k@gothfem.me>
2018-10-07 16:56:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function  lowerCaseDrivers (array) {
+  return array.map(function (driver) { return driver.toLowerCase() })
+}
+
+function nameToAttributes (drivers) {
+  return drivers.map(function (driver) {
+    const array = driver.split(` `)
+    return { firstName: array[0], lastName: array[1] }
+  })
+}
+
+function attributesToPhrase (drivers) {
+
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
